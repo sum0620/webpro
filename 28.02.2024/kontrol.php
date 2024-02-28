9a7086253854a2a4d0a5e914bc080100f8faa959
sum0620 <146706608+sum0620@users.noreply.github.com>
2024-02-28 12:27:42 +0300

    Add files via upload

--- 28.02.2024/kontrol.php
+++ 28.02.2024/kontrol.php
@@ -7,6 +7,18 @@
 </head>
 <body>
 	<h1>TEBRİKLER</h1>
+	<?php
+		$tc=$_POST["tc"];
+		$adS=$_POST["adSoyad"];
+		$yas=$_POST['yas'];
+		$ePosta=$_POST["ePosta"];
+		$profil=$_POST['profil'];
+		$tarih=$_POST['tarih'];
+		$deneyim=$_POST['deneyim'];
+		$pDil=$_POST['pDil'];
+		$onay=$_POST['onay'];
+		echo "tc:$tc,adSoyad;$adS";
+			?>
 
 </body>
 </html>
